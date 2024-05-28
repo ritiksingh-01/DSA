9ed39d74ab1a38f4996828f6abaea6b3afdd7453
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2024-05-28 22:57:09 +0530

    Permutation Commit

--- String/PermutationString.cpp
+++ String/PermutationString.cpp
@@ -1,51 +1,55 @@
-#include<iostream>
-using namespace std ;
-bool checkEqual(int a[26] , int b[26]){
-    for(int i = 0 ; i <26 ; i++){
-        if(a[i] != b[i]){
-            return 0;
+#include <iostream>
+using namespace std;
+
+bool checkEqual(int a[26], int b[26]) {
+    for (int i = 0; i < 26; i++) {
+        if (a[i] != b[i]) {
+            return false;
         }
     }
-    return 1;
+    return true;
 }
-bool permutationString(string s1 , string s2){
+
+bool permutationString(string s1, string s2) {
     int count1[26] = {0};
-    for(int i = 0 ; i < s1.length() ; i++){
+    for (int i = 0; i < s1.length(); i++) {
         int index = s1[i] - 'a';
-        count1[index]++ ;
+        count1[index]++;
     }
 
     int i = 0;
     int windowSize = s1.length();
     int count2[26] = {0};
-    while( i < windowSize){
+    while (i < windowSize) {
         int index = s2[i] - 'a';
         count2[index]++;
         i++;
     }
 
-    if(checkEqual(count1 , count2)){
-        return 1;
+    if (checkEqual(count1, count2)) {
+        return true;
     }
-    while ( i < s2.length()){
+
+    while (i < s2.length()) {
         char newChar = s2[i];
         int index = newChar - 'a';
         count2[index]++;
-       char oldChar = s2[i - windowSize];
+        char oldChar = s2[i - windowSize];
         index = oldChar - 'a';
         count2[index]--;
+
+        if (checkEqual(count1, count2)) {
+            return true;
+        }
         i++;
-        if(checkEqual(count1 , count2)){
-        return 1;
     }
-    return 0;
-    }
+
+    return false;
 }
 
-int main () {
-string s1 = "ab";
-string s2 = "eidbaooo";
-
-cout << permutationString(s1 , s2);
+int main() {
+    string s1 = "ab";
+    string s2 = "eidbaooo";
+    cout << permutationString(s1, s2);
     return 0;
 }
