41f512b3c167cb6002cc53a52d02a3c3f1cad90e
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2024-06-02 22:31:39 +0530

    2DArray Commit

---
+++ 2D Array/WavePrint.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector <int> wavePrint(vector<vector<int>> arr , int n , int m){
+    vector <int> ans;
+    for(int j = 0 ; j < m ; j++){
+
+        if(j&1){
+            for(int i = n -1 ; i >= 0 ; i--){
+                ans.push_back(arr[i][j]);
+            }
+        }
+        else{
+            for(int i = 0 ; i < n ; i++){
+                ans.push_back(arr[i][j]);
+            }
+        }
+    }
+    return ans;
+}
+int main(){
+int n = 3, m = 4;
+vector<vector<int>> arr(n, vector<int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Array is :" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+cout << "Wave order is : " << endl ;
+vector <int> ans = wavePrint(arr , 3 ,4);
+for(int i : ans){
+    cout<< i << " ";
+}
+    return 0;
+}
