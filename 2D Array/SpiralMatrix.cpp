ad34487a33d73e5c37ad8fd65f97caaa72e6b09b
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2024-06-11 00:09:29 +0530

    2DArray Commit

--- 2D Array/SpiralMatrix.cpp
+++ 2D Array/SpiralMatrix.cpp
@@ -15,32 +15,44 @@ vector<int> spiralPrint(vector<vector<int>> matrix){
     int endingCol = col - 1;
     while( count < total){
 
-        for (int i = startingCol ; i <= endingCol ; i++){
+        for (int i = startingCol ;count < total && i <= endingCol ; i++){
             ans.push_back(matrix[startingRow][i]);
             count++;
         }
         startingRow++;
-        for(int i = startingRow ; i <= endingRow ; i++){
+        for(int i = startingRow ;count < total && i <= endingRow ; i++){
             ans.push_back(matrix[i][endingCol]);
             count++;
         }
         endingCol--;
-        for(int i = endingCol ; i >= startingCol ; i++){
+        for(int i = endingCol ;count < total && i >= startingCol ; i--){
             ans.push_back(matrix[endingRow][i]);
             count++;
         }
         endingRow--;
-        for(int i = endingRow ; i >= startingRow ; i--){
+        for(int i = endingRow ;count < total && i >= startingRow ; i--){
             ans.push_back(matrix[i][startingCol]);
             count++;
         }
-        startingCol++:
+        startingCol++;
     }
     return ans;
 }
 
 
 int main(){
+int n = 3, m = 3;
+vector<vector<int>> matrix(n, vector<int>(m));
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+        cin >> matrix[i][j];
+    }
+}
+cout << "Spiral Matrix is : " << endl ;
+vector <int> ans = spiralPrint(matrix);
+for(int i : ans){
+    cout<< i << " ";
+}
 
     return 0;
 }
