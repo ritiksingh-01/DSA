2ccbcc47a5398dc7a6e41bd9a90f889e6593feb0
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2024-06-10 23:52:07 +0530

    2DArray Commit

---
+++ 2D Array/SpiralMatrix.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> spiralPrint(vector<vector<int>> matrix){
+    int row = matrix.size();
+    int col = matrix[0].size();
+    int count = 0 ;
+    int total = row*col;
+
+    vector <int> ans;
+
+    int startingRow = 0;
+    int startingCol = 0;
+    int endingRow = row - 1 ;
+    int endingCol = col - 1;
+    while( count < total){
+
+        for (int i = startingCol ; i <= endingCol ; i++){
+            ans.push_back(matrix[startingRow][i]);
+            count++;
+        }
+        startingRow++;
+        for(int i = startingRow ; i <= endingRow ; i++){
+            ans.push_back(matrix[i][endingCol]);
+            count++;
+        }
+        endingCol--;
+        for(int i = endingCol ; i >= startingCol ; i++){
+            ans.push_back(matrix[endingRow][i]);
+            count++;
+        }
+        endingRow--;
+        for(int i = endingRow ; i >= startingRow ; i--){
+            ans.push_back(matrix[i][startingCol]);
+            count++;
+        }
+        startingCol++:
+    }
+    return ans;
+}
+
+
+int main(){
+
+    return 0;
+}
