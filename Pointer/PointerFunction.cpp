ec077388403bc456226df37b6c74b39eb526c82b
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2024-06-29 00:00:37 +0530

    Pointer Commit

---
+++ Pointer/PointerFunction.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void print(int *p){
+    cout << "Value is : " << *p << endl;
+}
+void update(int *p){
+    *p = *p + 1;
+}
+
+int getSum(int *arr,int n){
+    int sum = 0;
+    cout << endl << "Size : " << sizeof(arr) << endl;
+    for(int i = 0 ; i < n ; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+int main(){
+    int value = 5 ;
+    int *p = &value;
+    // print(p);
+    cout << "Before update : " << *p << endl;
+    update(p);
+    cout << "After update : " << *p << endl;
+
+   int arr[6] = {1,2,3,4,5,8};
+   cout << "Sum : " << getSum(arr+3,3) << endl;
+
+
+    return 0;
+}
