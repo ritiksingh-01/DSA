26c2e31934be8dd4bb106354abb2ce56d0579d23
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2024-07-03 23:50:36 +0530

    Pointer Commit

---
+++ Pointer/2DArray.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // dynamic memory allocation
+    int row ;
+    cin >> row;
+    int col ;
+    cin >> col;
+    int **arr = new int *[row];
+    for(int i = 0 ; i < row ; i++){
+        arr[i] = new int[col];
+    }
+
+    for(int i = 0 ; i < row ; i++){
+        for(int j = 0 ; j < col ; j++){
+            cin >> arr[i][j];
+        }
+    }
+    cout << "Array is : " << endl;
+    for(int i = 0 ; i < row ; i++){
+    for(int j = 0 ; j < col ; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout<<endl;
+    }
+
+    for(int i = 0 ; i < row ; i++){
+        delete [] arr[i];
+    }
+    delete [] arr;
+    return 0;
+}
