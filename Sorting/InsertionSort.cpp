1d159326d866b15b8d39a62556121949f3aa21a4
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2024-12-28 19:05:49 +0530

    Sorting

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 using namespace std;
 
+// approach 1
 void insertionSort(vector<int> &arr, int n) {
     for (int i = 1; i < n; i++) {
         int temp = arr[i];
@@ -15,12 +16,27 @@ void insertionSort(vector<int> &arr, int n) {
         arr[j + 1] = temp;
     }
 }
-
+//  approach 2
+void insertionSort2(vector<int> &arr, int n) {
+    for(int i = 0 ; i<= n-1 ; i++){
+        int j = i;
+        while(j > 0 && arr[j-1]>arr[j]){
+            swap(arr[j-1],arr[j]);
+            j--;
+        }
+    }
+}
 int main() {
     vector<int> arr = {2, 5, 1, 4, 5};
     int size = arr.size();
     insertionSort(arr, size);
-    cout << "Sorted Array is : ";
+    cout << "Sorted Array using approach 1 : ";
+    for (int i : arr) {
+        cout << i << " ";
+    }
+    cout << endl;
+    insertionSort2(arr, size);
+    cout << "Sorted Array using approach 2 : ";
     for (int i : arr) {
         cout << i << " ";
     }
--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+//  approach 1
 void bubbleSort(vector<int> &arr , int size ){
     for(int i = 1 ; i < size ; i++){
 
@@ -11,11 +12,35 @@ void bubbleSort(vector<int> &arr , int size ){
         }
     }
 }
+// approach 2 
+
+void bubblesort2(vector<int> &arr , int n){
+    int didswap = 0;
+    for(int i = n -1 ; i >= 1 ; i--){
+        for(int j = 0 ; i <= i - 1 ; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                didswap = 1;
+            }
+        }
+        if(didswap == 0){
+            break;
+        }
+    }
+}
 int main (){
 vector<int> arr = {2,5,1,4,5};
 int size = arr.size();
 bubbleSort(arr,size);
-cout << "Sorted Array is : ";
+
+cout << "Sorted Array using 1 approach : ";
+for (int i : arr){
+    cout << i <<" ";
+}
+cout << endl;
+vector<int> arr1 = {1,2,3,4,5,6};
+bubblesort2(arr, size);
+cout << "Sorted Array using 2 approach : ";
 for (int i : arr){
     cout << i <<" ";
 }
