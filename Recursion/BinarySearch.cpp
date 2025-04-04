5486076b7c283549a812520ddc42071c76600f76
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2025-04-05 00:02:01 +0530

    Binary Search recursion Commit

--- Recursion/BinarySearch.cpp
+++ Recursion/BinarySearch.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 bool binarySearch(int *arr , int s , int e , int k){
@@ -17,6 +17,21 @@ bool binarySearch(int *arr , int s , int e , int k){
 
 }
 
+int binary(vector<int> &arr , int s , int e , int x){
+    if(s>=e){
+        return -1;
+    }
+    int mid = s + (e-s)/ 2;
+    if(arr[mid] == x){
+        return mid;
+    }else if(arr[mid] > x){
+    return binary(arr,s,mid-1,x);
+    }
+    else{
+    return binary(arr,mid+1,e,x);
+    }
+}
+
 int main(){
     int arr[8] = {1,2,3,4,5,6,7,9};
     int size = 8;
@@ -28,4 +43,7 @@ int main(){
     else{
         cout << "Not Found" << endl;
     }
+    vector<int> arr1= {2,3,4,5,7,8,9,12};
+    cout << binary(arr1,0,arr1.size(),4);
+    return 0;
 }
