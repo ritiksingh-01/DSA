cc198e982bf44aa7718cd1ad856e06dcad76dece
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2025-04-10 00:11:30 +0530

    GenerateParentheses Commit

--- Recursion/GenerateParentheses.cpp
+++ Recursion/GenerateParentheses.cpp
@@ -1,22 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-vector<string> solve(int n , int left , int right , vector<string> &ans , string temp){
-
+void solve(int n, int left, int right, vector<string> &ans, string &temp) {
+    if (left + right == 2 * n) {
+        ans.push_back(temp);
+        return;
+    }
     
+    if (left < n) {
+        temp.push_back('(');
+        solve(n, left + 1, right, ans, temp); 
+        temp.pop_back();
+    }
+
+    if (right < left) {
+        temp.push_back(')');
+        solve(n, left, right + 1, ans, temp);
+        temp.pop_back();
+    }
 }
 
-int main(){
-
-
+int main() {
     int n = 3;
-    int l = n;
-    int r = 3;
     string temp;
-    vector<string> ans ;
-    solve(n,l,r,ans,temp);
-    for(int  i = 0 ; i < ans.size() ; i++){
+    vector<string> ans;
+    solve(n, 0, 0, ans, temp);
+    
+    for (int i = 0; i < ans.size(); i++) {
         cout << ans[i] << endl;
-    }
+    } 
+    
+    return 0;
 }
