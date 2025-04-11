e0242100ff920de39cc0a3c689e4a1eccab85991
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2025-04-11 22:56:05 +0530

    subsetSum commit

--- Recursion/subsetSum.cpp
+++ Recursion/subsetSum.cpp
@@ -16,6 +16,17 @@ void subsetSum(vector<int> &arr, int i, int n, vector<int> &ans, vector<int> tem
     subsetSum(arr, i + 1, n, ans, temp); // Yes
 }
 
+
+void subsetSum2(vector<int> &arr, int i, int n, vector<int> &ans,int sum) {
+    if (i == n) {
+        ans.push_back(sum);
+        return;
+    }
+    subsetSum2(arr, i + 1, n, ans, sum);  // NO
+    sum += arr[i];
+    subsetSum2(arr, i + 1, n, ans, sum); // Yes
+}
+
 int main() {
     vector<int> arr = {1, 2, 3};
     int n = arr.size();
@@ -25,6 +36,14 @@ int main() {
     subsetSum(arr, 0, n, ans, temp);
     sort(ans.begin() , ans.end());
     for(int i : ans){
+        cout << i <<  " " ;
+    }
+    cout << endl;
+    int  sum = 0;
+    vector<int> ans2;
+    subsetSum2(arr, 0, n, ans2, sum);
+    sort(ans2.begin() , ans2.end());
+    for(int i : ans2){
         cout << i <<  " " ;
     }
     
