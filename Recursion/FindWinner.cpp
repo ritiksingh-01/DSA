7ac2b8ac72dd9dd2cd08f84af42f0d4c1967bd5f
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2025-04-24 23:52:01 +0530

    FindWinner 2 Commit

--- Recursion/FindWinner.cpp
+++ Recursion/FindWinner.cpp
@@ -1,20 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int RR(int n, int k) {
-    if (n == 1) return 0;
-    return (RR(n - 1, k) + k) % n;
-}
+// int RR(int n, int k) {
+//     if (n == 1) return 0;
+//     return (RR(n - 1, k) + k) % n;
+// }
 
-int findTheWinner(int n, int k) {
-    int ans = RR(n, k) + 1;
-    return ans;
+// int findTheWinner(int n, int k) {
+//     int ans = RR(n, k) + 1;
+//     return ans;
+// }
+
+int winner(vector<bool> &person , int n , int index , int personLeft , int k){
+    if(personLeft == 1){
+        for(int i = 0 ; i < n ; i++){
+            if(person[i] == 0){
+                return i;
+            }
+        }
+    }
+
+    int kill = (k-1) % personLeft;
+    while(kill--){
+        index = (index+1) % n;
+        while(person[index] == 1){
+            index = (index+1) % n;
+        }
+    }
+    person[index] = 1;
+    while(person[index] == 1){
+        index = (index - 1) % n;
+    }
+
+    return winner(person , n , index, personLeft-1 , k);
 }
 
 int main() {
     int n = 5;
     int k = 2; 
-    int winner = findTheWinner(n, k);
-    cout << "The winner is: " << winner << endl;
+    vector<bool> person(n,0);
+    int ans = winner(person , n , 0 ,n,k)+1;
+    cout << "The winner is: " << ans << endl;
     return 0;
 }
