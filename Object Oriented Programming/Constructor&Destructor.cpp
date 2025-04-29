d6d0f12afa5027ef658396818be143e119b543d2
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2025-04-29 23:56:48 +0530

    Destructor Commit

--- Object Oriented Programming/Constructor&Destructor.cpp
+++ Object Oriented Programming/Constructor&Destructor.cpp
@@ -3,29 +3,72 @@ using namespace std;
 
 class Customer{
     public:
+    // string name ;
+    // int account_number ;
+    // int balance ;
+
+
+    // // Default constructor
+    // Customer(){
+    //     cout << "Constructor is called " ;
+    // }
+
+    // // Parameterized constructor
+    // // Customer(string str , int a , int b){
+    // //     name = str;
+    // //     account_number = a;
+    // //     balance = b;
+    // // }
+    // // constructor overloading
+    // Customer(string name1 , int a){
+    //     name = name1;
+    //     account_number = a;
+
+    // }
+
+    // // Inline constructor 
+    // inline Customer(string s , int a , int b) : name(s) , account_number(a) , balance(b){
+
+    // }
+
+    // // copy constructor 
+
+    // Customer(Customer &B){
+    //     name = B.name;
+    //     account_number = B.account_number;
+    //     balance = B.balance;
+    // }
+
+    // destructor
     string name ;
-    int account_number ;
-    int balance ;
-
-
-    // Default constructor
-    Customer(){
-        cout << "Constructor is called " ;
+    int * balance;
+    Customer(string name , int bal){
+        this -> name = name ;
+        balance = new int;
+        *balance = bal;
+    }
+    ~Customer(){
+        cout << name << endl;
+        delete balance;
     }
 
-    // Parameterized constructor
-    Customer(string str , int a , int b){
-        name = str;
-        account_number = a;
-        balance = b;
-    }
+    void display(){
+        cout << name <<" "<<balance << endl;
+    } 
 };
 
 
 int main(){
 
-    Customer obj ("Ritik" , 200034 , 200);
+    // Customer obj ("Ritik" , 20003 , 200);
+    // Customer obj1 ("Rishi" , 20004);
+    // obj.display();
+    // obj1.display();
 
-    cout << obj.name << endl;
+    // Customer obj2(obj);
+    // obj2.display();
+
+    Customer obj1 ("Rishi" , 20004);
+    // obj1.display();
     return 0;
 }
