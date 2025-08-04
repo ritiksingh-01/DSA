8b2b58bc316b45615e72cd3baeeac1d9f58e44a7
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2025-08-04 23:45:52 +0530

    Optimal solution of Largest Rectangle In Histogram

--- Stack/LargestRectangleInHistogram.cpp
+++ Stack/LargestRectangleInHistogram.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // time complexity
 //  tc = O(n)
 // sc = O(n)
+//  brute force
 void largestRectangleArea(vector<int> &heights)
 {
     int n = heights.size();
@@ -47,9 +47,50 @@ void largestRectangleArea(vector<int> &heights)
     cout << ans << endl;
 }
 
+// optimal
+
+void largestRectangleArea1(vector<int> &heights)
+{
+    int n = heights.size();
+    stack<int> st;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && heights[st.top()] > heights[i])
+        {
+            int index = st.top();
+            st.pop();
+            if (!st.empty())
+            {
+                ans = max(ans, heights[index] * (i - st.top() - 1));
+            }
+            else
+            {
+                ans = max(ans, heights[index] * i);
+            }
+        }
+        st.push(i);
+    }
+    while (!st.empty())
+    {
+        int index = st.top();
+        st.pop();
+        if (!st.empty())
+        {
+            ans = max(ans, heights[index] * (n - st.top() - 1));
+        }
+        else
+        {
+            ans = max(ans, heights[index] * (n));
+        }
+    }
+    cout <<  ans << endl;
+}
+
 int main()
 {
-    vector<int> arr = {2,1,5,6,2,3};
+    vector<int> arr = {2, 1, 5, 6, 2, 3};
     largestRectangleArea(arr);
+    largestRectangleArea1(arr);
     return 0;
 }
