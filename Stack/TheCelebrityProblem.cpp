a75403b0debc6a9f4eb2c83c8ddbe292d2a642f8
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2025-08-10 16:36:02 +0530

    Solve The Celebrity Problem

---
+++ Stack/TheCelebrityProblem.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int celebrity(vector<vector<int>> &mat)
+{
+    int n = mat.size();
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        st.push(i);
+    }
+    while (st.size() > 1)
+    {
+        int first = st.top();
+        st.pop();
+        int second = st.top();
+        st.pop();
+
+        if (mat[first][second])
+        {
+            st.push(second);
+        }
+        else
+        {
+            st.push(first);
+        }
+    }
+
+    if (st.empty())
+        return -1;
+    int num = st.top();
+    st.pop();
+    for (int i = 0; i < n; i++)
+    {
+        if (i != num && (mat[num][i] || !mat[i][num]))
+        {
+            return -1;
+        }
+    }
+
+    return num;
+}
+
+int main()
+{
+    vector<vector<int>> mat = {{1, 1, 0}, {0, 1, 0}, {0, 1, 1}};
+    cout << celebrity(mat);
+    return 0;
+}
