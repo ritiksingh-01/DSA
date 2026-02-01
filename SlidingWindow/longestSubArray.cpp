17d8717c7f2a14fd300035678e620651bba5fd8d
Ritik Singh <144662342+Ritiksingh02@users.noreply.github.com>
2026-02-01 20:56:07 +0530

    Intro sliding window

---
+++ SlidingWindow/longestSubArray.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// better solution
+
+int subArray(vector<int> arr , int n , int k){
+    int l = 0 , r = 0 , sum = 0 , maxLen = 0;
+    while(r < n){
+        sum  = sum + arr[r];
+        while(sum > k){
+            sum = sum - arr[l];
+            l++;
+        }
+        if(sum <= k){
+            maxLen = max(maxLen , r - l + 1);
+            r++;
+        }
+    }
+    return maxLen;
+}
+
+// optimal solution
+
+int subArray1(vector<int> arr , int n , int k){
+    
+}
+
+
+int main(){
+
+
+    int n , k;
+    cin >> n;
+    cin >> k;
+    vector<int> arr(n);
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+    int len = subArray(arr, n , k);
+    cout << len << endl;
+
+    return 0;
+
+}
